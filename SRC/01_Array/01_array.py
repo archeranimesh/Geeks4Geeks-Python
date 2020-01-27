d1c0aa691224b014fa0f3b6351a857ce1dcd9939
archeranimesh <animeshb@archerimagine.com>
2020-01-27 14:00:56 +0530

    First example for array is completed

--- SRC/01_Array/01_array.py
+++ SRC/01_Array/01_array.py
@@ -1,3 +1,33 @@
 import array
 
-arr = array.array('i),[1,2,3]
+# Intialize the array with signed int
+arr = array.array('i',[1,2,3])
+
+
+# Print the original array
+print("The new created array is : ", end=" ")
+for i in range(0,3):
+    print(arr[i], end=" ")
+
+print("\r")
+
+# using append() to insert new value at the end
+arr.append(4)
+
+print("The appended array is: ", end=" ")
+for i in range(0,4):
+    print(arr[i], end=" ")
+
+print("\r")
+
+
+# using insert() to insert value at a specific index.
+arr.insert(2,5)
+
+
+print("The inserted array is: ", end=" ")
+for i in range(0,5):
+    print(arr[i], end=" ")
+
+print("\r")
+
---
+++ SRC/01_Array/02_array.py
@@ -0,0 +1,10 @@
+import array
+
+arr = array.array("i", [1,2,3,1,5])
+
+# print the original list
+for i in range(0,5):
+    print(arr[i], end=" ")
+print("\n")
+
+
