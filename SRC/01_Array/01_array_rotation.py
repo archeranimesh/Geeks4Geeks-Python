1ebfe2afad440e0b247945cdf7c398b664cc5eae
archeranimesh <animeshb@archerimagine.com>
2020-01-28 10:16:35 +0530

    Improved version for array rotation.

--- SRC/01_Array/01_array_rotation.py
+++ SRC/01_Array/01_array_rotation.py
@@ -3,9 +3,9 @@ arr = [1,2,3,4,5,6,7]
 n = len(arr)
 d = 3
 
+def leftRotate(arr, d, n):
+    return (arr[d:] + arr[0:d])
+
 print("n = ", n, "d = ", d)
-new_arr = arr[0:d]
-print(new_arr)
-arr = arr[d:]
-arr.extend(new_arr)
-print(arr)
+
+print(leftRotate(arr, d, n))
