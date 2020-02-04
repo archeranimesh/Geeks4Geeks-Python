251d4569a5d1d70e4bc1406673cb5f9c9e6a49b4
archeranimesh <animeshb@archerimagine.com>
2020-02-04 13:53:54 +0530

    Array rotation using reversing an array

---
+++ SRC/01_Array/03_array_rotation.py
@@ -0,0 +1,36 @@
+# This is the same array rotation program using reversing an Array.
+
+# Reverse the array within the start and end index
+def reverse_array(arr, start, end):
+    while(start < end):
+        temp = arr[start]
+        arr[start] = arr[end]
+        arr[end] = temp
+        start += 1
+        end -= 1
+
+
+def rotate_left(arr, d):
+    if d == 0 :
+        return
+    n = len(arr)
+    reverse_array(arr, 0, d - 1)
+    reverse_array(arr, d, n - 1)
+    reverse_array(arr, 0, n - 1)
+
+
+# Print the array
+def print_array(arr):
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+    print("\n")
+
+if __name__ == "__main__":
+    arr = [1,2,3,4,5,6,7]
+    d = 2
+    print_array(arr)
+#    reverse_array(arr, 0, len(arr) - 1)
+    print_array(arr)
+    rotate_left(arr, d)
+    print_array(arr)
+
